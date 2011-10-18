0f8834abfa0291615709dcf382143d12a47a897e
Makarand Karvekar <Makarand.Karvekar@motorola.com>
2011-10-18 13:14:26 -0500

    IKQCOMSD1-69: add touch board file for melfas driver
    Melfas configuration data and platform init.
    
    Change-Id: I44512bf4e5649a5fb6429f6998f34028170c3012
    Signed-off-by: Makarand Karvekar <Makarand.Karvekar@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/289965
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Igor Kovalenko <Igor.Kovalenko@motorola.com>

--- arch/arm/mach-msm/board-msm8960.h
+++ arch/arm/mach-msm/board-msm8960.h
@@ -41,4 +41,6 @@ extern struct regulator_init_data msm_saw_regulator_pdata_s6;
 
 extern struct rpm_regulator_platform_data msm_rpm_regulator_pdata __devinitdata;
 
+extern int __init melfas_ts_platform_init(void);
+
 #endif
