03d906f8cd89453b9e04d0ddb3e0a630752f5b82
Jeffrey Carlyle <jeff.carlyle@motorola.com>
2012-01-06 17:09:34 -0600

    IKKRNBSP-875 msm8690: asanti: move I2C peripherals to device tree
    Change-Id: I6fbd3bf32501505eb2910353e201d863c154d17a
    Signed-off-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/330526
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Mark Melone <g15829@motorola.com>

--- arch/arm/mach-msm/board-msm8960-mmi.c
+++ arch/arm/mach-msm/board-msm8960-mmi.c
@@ -2355,10 +2355,6 @@ static __init void asanti_init(void)
 	otg_control_data = NULL;
 	pm8921_gpios = pm8921_gpios_asanti;
 	pm8921_gpios_size = ARRAY_SIZE(pm8921_gpios_asanti);
-	ENABLE_I2C_DEVICE(TOUCHSCREEN_ATMEL);
-	ENABLE_I2C_DEVICE(CAMERA_MSM);
-	ENABLE_I2C_DEVICE(ALS_CT406);
-	ENABLE_I2C_DEVICE(BACKLIGHT_LM3532);
 	keypad_data = &mmi_qwerty_keypad_data;
 	keypad_mode = MMI_KEYPAD_RESET|MMI_KEYPAD_SLIDER;
 
