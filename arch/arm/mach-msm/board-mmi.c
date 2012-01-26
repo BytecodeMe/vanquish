9cbea2b4d315a8014d113e7983b27c9d051fab27
Alexander Filonenko <vtp684@motorola.com>
2012-01-26 12:24:23 -0600

    IKQCOMSD1-773 Add support to get LPDDR2 memory info in kernel
    Expose DDR info passed from SBL3 over shared memory under /sys/ram.
    
    Change-Id: Ia2635064c0ec97d3767fa00a7756aac0b6841cf1
    Signed-off-by: Alexander Filonenko <vtp684@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/341531
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Mark Melone <g15829@motorola.com>

--- arch/arm/mach-msm/board-mmi.c
+++ arch/arm/mach-msm/board-mmi.c
@@ -1428,6 +1428,175 @@ static void init_mmi_unit_info(void){
 		mui->system_serial_low, mui->machine, mui->barcode);
 }
 
+static struct {
+	unsigned mr5;
+	unsigned mr6;
+	unsigned mr7;
+	unsigned mr8;
+	unsigned ramsize;
+} *smem_ddr_info;
+
+static ssize_t sysfsram_mr_register_show(struct kobject *kobj,
+			struct kobj_attribute *attr, char *buf)
+{
+	const char *name;
+	if (smem_ddr_info == NULL)
+		return snprintf(buf, 20, "smem_alloc failed\n");
+
+	name = attr->attr.name;
+	if (strnlen(name, 4) == 3 && name[0] == 'm' && name[1] == 'r') {
+		switch (name[2]) {
+		case '5':
+			return snprintf(buf, 6, "0x%02x\n", smem_ddr_info->mr5);
+
+		case '6':
+			return snprintf(buf, 6, "0x%02x\n", smem_ddr_info->mr6);
+
+		case '7':
+			return snprintf(buf, 6, "0x%02x\n", smem_ddr_info->mr7);
+
+		case '8':
+			return snprintf(buf, 6, "0x%02x\n", smem_ddr_info->mr8);
+		}
+	}
+
+	return snprintf(buf, 20, "unknown attribute\n");
+}
+
+static ssize_t sysfsram_size_show(struct kobject *kobj,
+			struct kobj_attribute *attr, char *buf)
+{
+	if (smem_ddr_info != NULL)
+		return snprintf(buf, 12, "0x%08x\n", smem_ddr_info->ramsize);
+
+	return snprintf(buf, 20, "smem_alloc failed\n");
+}
+
+static ssize_t sysfsram_serial_show(struct kobject *kobj,
+			struct kobj_attribute *attr, char *buf)
+{
+	return snprintf(buf, 3, "0\n");
+}
+
+static ssize_t sysfsram_info_show(struct kobject *kobj,
+			struct kobj_attribute *attr, char *buf)
+{
+	const char *vname = "Reserved";
+	uint32_t vid;
+	static const char *vendors[] = {
+		"Reserved",
+		"Samsung",
+		"Qimonda",
+		"Elpida",
+		"Etron",
+		"Nanya",
+		"Hynix",
+		"Mosel",
+		"Winbond",
+		"ESMT",
+		"Reserved",
+		"Spansion",
+		"SST",
+		"ZMOS",
+		"Intel"
+	};
+
+	if (smem_ddr_info == NULL)
+		return snprintf(buf, 20, "smem_alloc failed\n");
+
+	vid = smem_ddr_info->mr5 & 0xFF;
+	if (vid <= (sizeof(vendors)/sizeof(vendors[0])))
+		vname = vendors[vid];
+	else if (vid == 0xFE)
+		vname = "Numonyx";
+	else if (vid == 0xFF)
+		vname = "Micron";
+
+	return snprintf(buf, 40, "%s:%uMB\n", vname, smem_ddr_info->ramsize);
+}
+
+static ssize_t sysfsram_type_show(struct kobject *kobj,
+			struct kobj_attribute *attr, char *buf)
+{
+	const char *tname;
+	uint32_t tid;
+	static const char *types[] = {
+		"S4 SDRAM",
+		"S2 SDRAM",
+		"N NVM",
+		"Reserved"
+	};
+
+	if (smem_ddr_info == NULL)
+		return snprintf(buf, 20, "smem_alloc failed\n");
+
+	tid = smem_ddr_info->mr8 & 0x03;
+	if (tid <= (sizeof(types)/sizeof(types[0])))
+		tname = types[tid];
+
+	return snprintf(buf, 10, "%s\n", tname);
+}
+
+static struct kobj_attribute ddr_mr5_register_attr =
+	__ATTR(mr5, 0444, sysfsram_mr_register_show, NULL);
+
+static struct kobj_attribute ddr_mr6_register_attr =
+	__ATTR(mr6, 0444, sysfsram_mr_register_show, NULL);
+
+static struct kobj_attribute ddr_mr7_register_attr =
+	__ATTR(mr7, 0444, sysfsram_mr_register_show, NULL);
+
+static struct kobj_attribute ddr_mr8_register_attr =
+	__ATTR(mr8, 0444, sysfsram_mr_register_show, NULL);
+
+static struct kobj_attribute ddr_size_attr =
+	__ATTR(size, 0444, sysfsram_size_show, NULL);
+
+static struct kobj_attribute ddr_serial_attr =
+	__ATTR(serial, 0444, sysfsram_serial_show, NULL);
+
+static struct kobj_attribute ddr_type_attr =
+	__ATTR(type, 0444, sysfsram_type_show, NULL);
+
+static struct kobj_attribute ddr_info_attr =
+	__ATTR(info, 0444, sysfsram_info_show, NULL);
+
+static struct attribute *ram_info_properties_attrs[] = {
+	&ddr_mr5_register_attr.attr,
+	&ddr_mr6_register_attr.attr,
+	&ddr_mr7_register_attr.attr,
+	&ddr_mr8_register_attr.attr,
+	&ddr_size_attr.attr,
+	&ddr_serial_attr.attr,
+	&ddr_type_attr.attr,
+	&ddr_info_attr.attr,
+	NULL
+};
+
+static struct attribute_group ram_info_properties_attr_group = {
+	.attrs = ram_info_properties_attrs,
+};
+
+static void init_mmi_ram_info(void){
+	int rc = 0;
+	static struct kobject *ram_info_properties_kobj;
+
+	smem_ddr_info = smem_alloc(SMEM_SDRAM_INFO, sizeof(*smem_ddr_info));
+	if (!smem_ddr_info) {
+		pr_err("%s: failed to access DDR info in SMEM\n", __func__);
+		return;
+	}
+
+	ram_info_properties_kobj = kobject_create_and_add("ram", NULL);
+
+	if (ram_info_properties_kobj)
+		rc = sysfs_create_group(ram_info_properties_kobj,
+			&ram_info_properties_attr_group);
+
+	if (!ram_info_properties_kobj || rc)
+		pr_err("%s: failed to create /sys/ram\n", __func__);
+}
+
 static struct platform_device *mmi_devices[] __initdata = {
 	&msm8960_w1_gpio_device,
 	&msm_8960_q6_lpass,
@@ -2385,6 +2554,7 @@ static void __init msm8960_mmi_init(void)
 	BUG_ON(msm_pm_boot_init(&msm_pm_boot_pdata));
 
 	init_mmi_unit_info();
+	init_mmi_ram_info();
 }
 
 static int __init mot_parse_atag_baseband(const struct tag *tag)
