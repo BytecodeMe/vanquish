82800086261343a841d237382c1112ecba2fb2f1
Jason Hrycay <jason.hrycay@motorola.com>
2012-02-27 18:43:00 -0600

    IKQCOMSD1-1424: ORDOG Bringup
    Add ORDOG machine type to board init files.
    
    Change-Id: I8b10df994532bcc8f23b273f9571b8d24e944919
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/361058
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Christopher Fries <C.Fries@motorola.com>
    Reviewed-by: Igor Kovalenko <Igor.Kovalenko@motorola.com>

--- arch/arm/mach-msm/board-mmi.c
+++ arch/arm/mach-msm/board-mmi.c
@@ -3302,6 +3302,17 @@ MACHINE_START(ASANTI, "Asanti")
 	.fixup = msm8960_fixup,
 MACHINE_END
 
+MACHINE_START(ORDOG, "Ordog")
+	.map_io = msm8960_map_io,
+	.reserve = msm8960_reserve,
+	.init_irq = msm8960_init_irq,
+	.handle_irq = gic_handle_irq,
+	.timer = &msm_timer,
+	.init_machine = msm8960_mmi_init,
+	.init_early = mmi_init_early,
+	.init_very_early = msm8960_early_memory,
+MACHINE_END
+
 /* for use by products that are completely configured through device tree */
 MACHINE_START(MSM8960DT, "msm8960dt")
 	.map_io = msm8960_map_io,
