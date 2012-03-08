6b2d35c07c56e05b7dafd2c8356a517fcc0ea4a3
Alexander Filonenko <vtp684@motorola.com>
2012-03-08 16:28:33 -0600

    IKQCOMSD1-1452 Increase BASEBAND field size
    Increase the size of BASEBAND field being passed to modem over SMEM
    to accomodate new content
    
    Change-Id: I4267bd353dc30100e48b319b1ea827cb8711f9c0
    Signed-off-by: Alexander Filonenko <vtp684@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/369328
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>

--- arch/arm/mach-msm/smd_private.h
+++ arch/arm/mach-msm/smd_private.h
@@ -211,7 +211,7 @@ void smd_diag(void);
 #define BARCODE_MAX_LEN 64
 #define MACHINE_MAX_LEN 32
 #define CARRIER_MAX_LEN 64
-#define BASEBAND_MAX_LEN 32
+#define BASEBAND_MAX_LEN 96
 struct mmi_unit_info_v1 {
 	uint32_t version;
 	uint32_t system_rev;
