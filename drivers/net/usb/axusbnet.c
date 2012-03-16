049569a7c3ac0dd4078a54fd32245a086435b00e
Neil Patel <NPATEL@motorola.com>
2012-03-16 11:14:52 -0500

    IKHSS7-16016: Fix ethernet dongle disconnect hang
    When disconnecting the ASIX USB Ethernet dongle the unregister()
    was blocked waiting for flush_scheduled_work() to complete. The
    function was being called from a worker context which made this
    unsafe.
    
    Instead of using flush_scheduled_work() we can cancel the
    specific work item that needs to be flushed. This change has
    been applied to the other usbnet drivers via this commit:
    
       23f333a drivers/net: don't use flush_scheduled_work()
    
    Change-Id: I0a474c33fee851f9e2ebac69e548458058679c38
    Signed-off-by: Neil Patel <NPATEL@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/374761
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>

--- drivers/net/usb/axusbnet.c
+++ drivers/net/usb/axusbnet.c
@@ -1194,8 +1194,7 @@ void axusbnet_disconnect (struct usb_interface *intf)
 	net = dev->net;
 	unregister_netdev (net);
 
-	/* we don't hold rtnl here ... */
-	flush_scheduled_work ();
+	cancel_work_sync(&dev->kevent);
 
 	if (dev->driver_info->unbind)
 		dev->driver_info->unbind (dev, intf);
