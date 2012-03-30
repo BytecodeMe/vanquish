5b60bfdce3907ec18aaff48203c4ec6a937663c6
Joonwoo Park <joonwoop@codeaurora.org>
2012-03-30 16:44:58 -0700

    ASoC: msm8960: Add module MBHC module parameters
    Add boolean module parameters hs_detect_use_gpio and hs_detect_use_firmware
    The hs_detect_use_gpio enables MBHC mechanical detection,
    hs_detect_use_firmware makes codec driver to read MBHC calibration firmware
    from filesystem.  Both of them are set to false by default.
    
    Change-Id: I2c5f526a0b199265ebf85003cdc49e85fc76f5b8
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    (cherry picked from commit b727b09aaf3fce379c9e9fcb15ce595fbf0edb0f)

--- sound/soc/msm/msm8960.c
+++ sound/soc/msm/msm8960.c
@@ -74,7 +74,6 @@ static struct notifier_block emu_accy_notifier = {
 
 #define JACK_DETECT_GPIO 38
 #define JACK_DETECT_INT PM8921_GPIO_IRQ(PM8921_IRQ_BASE, JACK_DETECT_GPIO)
-#define GPIO_DETECT_USED false
 
 static u32 top_spk_pamp_gpio  = PM8921_GPIO_PM_TO_SYS(18);
 static u32 bottom_spk_pamp_gpio = PM8921_GPIO_PM_TO_SYS(19);
@@ -95,6 +94,14 @@ static int msm8960_headset_gpios_configured;
 
 static struct snd_soc_jack hs_jack;
 static struct snd_soc_jack button_jack;
+
+static bool hs_detect_use_gpio;
+module_param(hs_detect_use_gpio, bool, 0444);
+MODULE_PARM_DESC(hs_detect_use_gpio, "Use GPIO for headset detection");
+
+static bool hs_detect_use_firmware;
+module_param(hs_detect_use_firmware, bool, 0444);
+MODULE_PARM_DESC(hs_detect_use_firmware, "Use firmware for headset detection");
 
 static int msm8960_enable_codec_ext_clk(struct snd_soc_codec *codec, int enable,
 					bool dapm);
@@ -729,9 +736,9 @@ static int msm8960_audrx_init(struct snd_soc_pcm_runtime *rtd)
 	struct tabla_pdata *pdata = dev_get_platdata(codec->dev->parent);
 	struct pm_gpio jack_gpio_cfg = {
 		.direction = PM_GPIO_DIR_IN,
-		.pull = PM_GPIO_PULL_UP_1P5,
+		.pull = PM_GPIO_PULL_NO,
 		.function = PM_GPIO_FUNC_NORMAL,
-		.vin_sel = 2,
+		.vin_sel = PM_GPIO_VIN_L17,
 		.inv_int_pol = 0,
 	};
 
@@ -801,6 +808,9 @@ static int msm8960_audrx_init(struct snd_soc_pcm_runtime *rtd)
 			return err;
 		}
 	}
+
+	mbhc_cfg.read_fw_bin = hs_detect_use_firmware;
+
 	err = tabla_hs_detect(codec, &mbhc_cfg);
 
 	return err;
