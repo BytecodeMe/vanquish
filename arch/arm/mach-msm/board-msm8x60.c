8a463a5f843ec2f2891eb9b948c9724b45d61a8b
Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>
2012-04-12 16:23:20 -0700

    msm: camera: OV7692 v4l2 sensor driver.
    Add board file configuration, clock configuration, regulator
    setting and sensor register setting to enable OV7692 v4l2
    sensor driver
    
    CRs-fixed: 351530
    Change-Id: Iff3d65cd388c48491391853e71fce2817f3b8c6b
    Signed-off-by: Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>
    Signed-off-by: Roja Rani Yarubandi <rojay@codeaurora.org>
    
    Conflicts:
    	drivers/media/video/msm/sensors/msm_sensor.c

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -3864,6 +3864,7 @@ static struct regulator_consumer_supply vreg_consumers_PM8058_L15[] = {
 	REGULATOR_SUPPLY("8058_l15",		NULL),
 	REGULATOR_SUPPLY("cam_vana",		"1-001a"),
 	REGULATOR_SUPPLY("cam_vana",		"1-006c"),
+	REGULATOR_SUPPLY("cam_vana",		"1-0078"),
 };
 static struct regulator_consumer_supply vreg_consumers_PM8058_L16[] = {
 	REGULATOR_SUPPLY("8058_l16",		NULL),
@@ -3896,6 +3897,7 @@ static struct regulator_consumer_supply vreg_consumers_PM8058_L25[] = {
 	REGULATOR_SUPPLY("8058_l25",		NULL),
 	REGULATOR_SUPPLY("cam_vdig",		"1-001a"),
 	REGULATOR_SUPPLY("cam_vdig",		"1-006c"),
+	REGULATOR_SUPPLY("cam_vdig",		"1-0078"),
 };
 static struct regulator_consumer_supply vreg_consumers_PM8058_S0[] = {
 	REGULATOR_SUPPLY("8058_s0",		NULL),
@@ -3916,6 +3918,7 @@ static struct regulator_consumer_supply vreg_consumers_PM8058_LVS0[] = {
 	REGULATOR_SUPPLY("8058_lvs0",		NULL),
 	REGULATOR_SUPPLY("cam_vio",			"1-001a"),
 	REGULATOR_SUPPLY("cam_vio",			"1-006c"),
+	REGULATOR_SUPPLY("cam_vio",			"1-0078"),
 };
 static struct regulator_consumer_supply vreg_consumers_PM8058_LVS1[] = {
 	REGULATOR_SUPPLY("8058_lvs1",		NULL),
