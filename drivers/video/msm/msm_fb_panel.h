2e1c5e118013d2b76fa6f915fbf57c91f2615dba
Siddhartha <agrawals@codeaurora.org>
2012-05-07 18:58:56 -0700

    msm_fb: display: Update video format on fps change
    Added check for frame rate change. Corner case if there is no
    change in xres,yres and pixclock.
    
    Change-Id: I0ff5eb65364d2a370e0973da7e84f738bd042a20
    CRs-Fixed: 356328
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/msm_fb_panel.h

--- drivers/video/msm/msm_fb_panel.h
+++ drivers/video/msm/msm_fb_panel.h
@@ -159,7 +159,7 @@ struct msm_panel_info {
 	__u32 is_3d_panel;
 	__u32 physical_width_mm;
 	__u32 physical_height_mm;
-
+	__u32 frame_rate;
 
 	struct mddi_panel_info mddi;
 	struct lcd_panel_info lcd;
