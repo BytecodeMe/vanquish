0aefd7a3949c3a42a44378eca7123ceba5c14245
Mark Reed <mreed1@motorola.com>
2012-05-18 19:32:08 -0500

    IKHSS7-30187 - Enable signature checking of the video FW.
    Required for Widevine level 1 certification.
    
    Change-Id: Id484b0747369cf555ff2d9f1c9ed8ee0aca951a5
    Reviewed-on: http://gerrit.pcs.mot.com/419225
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Mark Reed <mreed1@motorola.com>
    Reviewed-by: Dean Vogler <cdv001@motorola.com>

--- arch/arm/mach-msm/board-mmi.c
+++ arch/arm/mach-msm/board-mmi.c
@@ -2429,6 +2429,7 @@ static struct platform_device *mmi_devices[] __initdata = {
 	&msm_8960_q6_mss_fw,
 	&msm_8960_q6_mss_sw,
 	&msm_8960_riva,
+	&msm_pil_vidc,
 	&msm_pil_tzapps,
 	&msm8960_device_otg,
 	&msm8960_device_gadget_peripheral,
