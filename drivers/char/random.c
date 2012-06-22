37c93112b47f8b6d12ecc51ee386581188ffbce7
Peter Zijlstra <peterz@infradead.org>
2012-06-22 13:36:05 +0200

    random: Fix build errors

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -655,7 +655,6 @@ void add_device_randomness(const void *buf, unsigned int size)
 }
 EXPORT_SYMBOL(add_device_randomness);
 
-static struct timer_rand_state input_timer_state;
 
 /*
  * This function adds entropy to the entropy "pool" by using timing
