c52f24d2b1be401cafff88554cb2616a757c087e
Kuogee Hsieh <khsieh@codeaurora.org>
2012-07-29 08:56:58 -0700

    msm_fb: display: Add BLT support to dsi command mode panel
    At run time, mdp BLT (writeback) mode is enabled and disabled
    base on performance level to avoid mdp underrun. Move BLT
    enabled/disabled logic to pan display so that enabled/disable
    is done per commit instead of per queue. Since all panels
    are shared same logic, dsi video mode and lcdc panle BLT logic
    have been changed accordingly.
    
    Change-Id: Iee4854b1bb6cc6f249dce0c2ccb39d5b07abe858
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mipi_dsi_host.c
+++ drivers/video/msm/mipi_dsi_host.c
@@ -1163,8 +1163,6 @@ void mipi_dsi_cmd_backlight_tx(int level)
 
 	spin_lock_irqsave(&dsi_mdp_lock, flag);
 	dsi_mdp_busy = TRUE;
-	spin_unlock_irqrestore(&dsi_mdp_lock, flag);
-
 	led_pwm1[1] = (unsigned char)(level);
 	tp = &dsi_tx_buf;
 	cmd = &backlight_cmd;
@@ -1190,10 +1188,6 @@ void mipi_dsi_cmd_backlight_tx(int level)
 	wmb();
 	MIPI_OUTP(MIPI_DSI_BASE + 0x08c, 0x01);	/* trigger */
 	wmb();
-
-	spin_lock_irqsave(&dsi_mdp_lock, flag);
-	dsi_mdp_busy = FALSE;
-	complete(&dsi_mdp_comp);
 	spin_unlock_irqrestore(&dsi_mdp_lock, flag);
 }
 
@@ -1449,6 +1443,8 @@ int mipi_dsi_cmd_dma_tx(struct dsi_buf *tp)
 	int ret;
 	int dsi_status1 = 0, dsi_status2 = 0;
 
+	unsigned long flags;
+
 #ifdef DSI_HOST_DEBUG
 	int i;
 	char *bp;
@@ -1462,6 +1458,7 @@ int mipi_dsi_cmd_dma_tx(struct dsi_buf *tp)
 	pr_debug("\n");
 #endif
 
+	spin_lock_irqsave(&dsi_mdp_lock, flags);
 	tp->len += 3;
 	tp->len &= ~0x03;	/* multipled by 4 */
 
@@ -1479,6 +1476,7 @@ int mipi_dsi_cmd_dma_tx(struct dsi_buf *tp)
 	wmb();
 	MIPI_OUTP(MIPI_DSI_BASE + 0x08c, 0x01);	/* trigger */
 	wmb();
+	spin_unlock_irqrestore(&dsi_mdp_lock, flags);
 
 	if (wait_for_completion_timeout(&dsi_dma_comp,
 						msecs_to_jiffies(100)) == 0) {
@@ -1627,8 +1625,8 @@ irqreturn_t mipi_dsi_isr(int irq, void *ptr)
 
 	if (isr & DSI_INTR_CMD_DMA_DONE) {
 		mipi_dsi_mdp_stat_inc(STAT_DSI_CMD);
-		complete(&dsi_dma_comp);
 		spin_lock(&dsi_mdp_lock);
+		complete(&dsi_dma_comp);
 		dsi_ctrl_lock = FALSE;
 		mipi_dsi_disable_irq_nosync();
 		spin_unlock(&dsi_mdp_lock);
