4b9a779ca417a5a9f95bae250d5e2b6a5f75e917
Subhash Chandra Bose Naripeddy <snariped@codeaurora.org>
2012-08-17 21:17:40 -0700

    ASoC: msm: Add support timestamp update VCAP playback usecase.
    Compressed data in HDMI IN or VCAP usecase is propagated with
    timestamp from DSP with each buffer. For playback, the same or
    interpolated timestamp is updated with the corresponding buffer.
    DSP will render the buffer a) without any delay if timestamp mode is
    synchronous to absolute timestamp b) with a delay if timestamp is more
    than the absolute timestamp c) drop the buffer if the timestamp is
    less than the absolute timestamp. Add support for the same.
    
    Signed-off-by: Subhash Chandra Bose Naripeddy <snariped@codeaurora.org>
    (cherry picked from commit aa1f5e51dbc251dc758a1f762802d87d4f2128b7)
    (cherry picked from commit cb209b8f0bddcedaf26a181f20de696e4ad729bc)
    
    Change-Id: I5b7b2cb405f72ea9afa4751c6a92cde09f40d69c
    Signed-off-by: Sudhir Sharma <sudsha@codeaurora.org>
    
    Conflicts:
    
    	sound/soc/msm/msm-compr-q6.c

--- sound/soc/msm/msm-pcm-q6.h
+++ sound/soc/msm/msm-pcm-q6.h
@@ -82,4 +82,12 @@ struct msm_audio {
 	bool meta_data_mode;
 };
 
+struct output_meta_data_st {
+	uint32_t meta_data_length;
+	uint32_t frame_size;
+	uint32_t timestamp_lsw;
+	uint32_t timestamp_msw;
+	uint32_t reserved[12];
+};
+
 #endif /*_MSM_PCM_H*/
