ca1ba90436c9195cbfe8429d0a2eed5262ce304f
Jeevan Shriram <jshriram@codeaurora.org>
2012-09-15 21:05:29 +0530

    msm_fb: display: Add support vsync driven composition for 7x30
    This patch will queue multiple surfaces and commit those
    surfaces into mdp at same instance so that surfaces will
    be blended and displayed at same time for 7x30. Hardware vsync event
    is delivered to the user space frame work via uevent. Both
    queue and commit are controlled by frame work and synchonized
    with vsync event. Therefore frame rate will match with vsync rate.
    
    Change-Id: I59d672d12f69bd964eb59c78ba0b86ddec57fe74
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -2059,6 +2059,8 @@ static int mdp_off(struct platform_device *pdev)
 			mfd->panel.type == LCDC_PANEL ||
 			mfd->panel.type == LVDS_PANEL)
 		mdp4_lcdc_off(pdev);
+	else if (mfd->panel.type == MDDI_PANEL)
+		mdp4_mddi_off(pdev);
 
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
 	ret = panel_next_off(pdev);
@@ -2083,12 +2085,16 @@ static int mdp_on(struct platform_device *pdev)
 	if (mfd->panel.type == MIPI_CMD_PANEL) {
 		mdp_vsync_cfg_regs(mfd, FALSE);
 		mdp4_dsi_cmd_on(pdev);
-	} else if (mfd->panel.type == MIPI_VIDEO_PANEL)
+	} else if (mfd->panel.type == MIPI_VIDEO_PANEL) {
 		mdp4_dsi_video_on(pdev);
-	else if (mfd->panel.type == HDMI_PANEL ||
+	} else if (mfd->panel.type == HDMI_PANEL ||
 			mfd->panel.type == LCDC_PANEL ||
-			mfd->panel.type == LVDS_PANEL)
+			mfd->panel.type == LVDS_PANEL) {
 		mdp4_lcdc_on(pdev);
+	} else if (mfd->panel.type == MDDI_PANEL) {
+		mdp_vsync_cfg_regs(mfd, FALSE);
+		mdp4_mddi_on(pdev);
+	}
 
 	mdp_clk_ctrl(0);
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_OFF, FALSE);
@@ -2439,6 +2445,9 @@ static int mdp_probe(struct platform_device *pdev)
 		INIT_WORK(&mfd->vsync_resync_worker,
 			  mdp_vsync_resync_workqueue_handler);
 		mfd->hw_refresh = FALSE;
+
+		if(mfd->panel.type == MDDI_PANEL)
+			mdp4_mddi_rdptr_init(0);
 
 		if (mfd->panel.type == EXT_MDDI_PANEL) {
 			/* 15 fps -> 66 msec */
