2bfa824c134182b82704dd5b6e7aab2921c9b97a
Ken Zhang <kenz@codeaurora.org>
2012-10-05 23:40:40 -0400

    msm: display: allow fb open even in suspend state
    resume might be coming later, allow fb open sucessfully, only
    panel driver needs be delayed
    
    (cherry picked from commit 89380aac14332dd3ff39a3f0904bfed117458abc)
    
    Change-Id: Id24112f5bba7ba6290113987ebcc8ba6e4256419
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mipi_dsi.c

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -298,7 +298,8 @@ static int mipi_dsi_on(struct platform_device *pdev)
 		wmb();
 	}
 
-	ret = panel_next_on(pdev);
+	if (mfd->op_enable)
+		ret = panel_next_on(pdev);
 
 	mipi_dsi_op_mode_config(mipi->mode);
 
