79ceeff0b83419be8bde5a778041c9cc794be490
Aravind Venkateswaran <aravindh@codeaurora.org>
2012-11-07 18:27:23 -0800

    msm_fb: dtv: Serve device off in a separate thread
    DTV device should not be powered down until the audio engine has
    shutdown. This change moves dtv audio off sequence to a separate
    thread so that framebuffer close system call does not get blocked.
    
    CRs-Fixed: 412575
    Change-Id: I246a9e58f1367e653913773696510f375abf3d3a
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/msm_fb.c

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -585,6 +585,7 @@ static int msm_fb_suspend_sub(struct msm_fb_data_type *mfd)
 	mfd->suspend.sw_refreshing_enable = mfd->sw_refreshing_enable;
 	mfd->suspend.op_enable = mfd->op_enable;
 	mfd->suspend.panel_power_on = mfd->panel_power_on;
+	mfd->suspend.op_suspend = true;
 
 	if (mfd->op_enable) {
 		ret =
@@ -656,12 +657,10 @@ static int msm_fb_resume_sub(struct msm_fb_data_type *mfd)
 				      mfd->op_enable);
 		if (ret)
 			MSM_FB_INFO("msm_fb_resume: can't turn on display!\n");
-	} else {
-		if (pdata->power_ctrl)
-			pdata->power_ctrl(TRUE);
 	}
 
 	unlock_panel_mutex(mfd);
+	mfd->suspend.op_suspend = false;
 
 	return ret;
 }
