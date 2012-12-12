f35cebe355541f7f49629560506698bae937176d
Emerson Pinter <dev@pinter.com.br>
2012-12-12 18:00:26 -0200

    Replace mdp_core_clk_rate with mdp_max_clk
    Change-Id: Ifd142e3dfe7b06888b4b14328a28bdcfdcea7250

--- arch/arm/mach-msm/board-msm8960-common.c
+++ arch/arm/mach-msm/board-msm8960-common.c
@@ -1599,18 +1599,9 @@ struct msm_bus_scale_pdata mdp_bus_scale_pdata = {
 
 #endif
 
-static int mdp_core_clk_rate_table[] = {
-	85330000,
-	85330000,
-	160000000,
-	200000000,
-};
-
 struct msm_panel_common_pdata mdp_pdata = {
 	.gpio = MDP_VSYNC_GPIO,
-	.mdp_core_clk_rate = 85330000,
-	.mdp_core_clk_table = mdp_core_clk_rate_table,
-	.num_mdp_clk = ARRAY_SIZE(mdp_core_clk_rate_table),
+    .mdp_max_clk = 200000000,
 #ifdef CONFIG_MSM_BUS_SCALING
 	.mdp_bus_scale_table = &mdp_bus_scale_pdata,
 #endif
