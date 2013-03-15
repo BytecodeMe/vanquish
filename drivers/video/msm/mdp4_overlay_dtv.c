0099076908f9c1e9f98fe98abd20f5f05b4a5790
Mayank Chopra <makchopra@codeaurora.org>
2013-03-15 19:49:24 +0530

    msm_fb: display: Wait for DMAE for external interface (DTV)
    When calling FBIOPAN_DISPLAY for external, there is no wait
    which results in irregular fps when tested with fbtest.
    Enable DMAE wait to wait until DMAE is over thereby fixing
    irregular fps shown in fbtest results.
    
    Crs-fixed: 438078
    Change-Id: Iff528ccc17a1e4078c8825b35d4778a7d9e40e7f
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdp4_overlay_dtv.c

--- drivers/video/msm/mdp4_overlay_dtv.c
+++ drivers/video/msm/mdp4_overlay_dtv.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1189,8 +1189,8 @@ void mdp4_dtv_overlay(struct msm_fb_data_type *mfd)
 		mdp4_dtv_pipe_queue(0, pipe);
 	}
 
-	if (mdp4_dtv_pipe_commit(0, 0))
-		mdp4_dtv_wait4dmae(0);
-
+	mdp4_overlay_mdp_perf_upd(mfd, 1);
+	mdp4_dtv_pipe_commit(0, 1);
+	mdp4_overlay_mdp_perf_upd(mfd, 0);
 	mutex_unlock(&mfd->dma->ov_mutex);
 }
