f350d50e34b6671521b4773c3e0c6cef782f93ef
Andrew Warfield <hiasant@gmail.com>
2013-05-17 14:17:03 -0500

    Revert "msm8960: Add 194Mhz frequency step for fast binned CPU as it seems to be very common"
    This reverts commit 4b7980bba0b488689041400b46e596cdb10b27fc.

--- arch/arm/mach-msm/acpuclock-8960.c
+++ arch/arm/mach-msm/acpuclock-8960.c
@@ -519,7 +519,6 @@ static struct acpu_level acpu_freq_tbl_8960_kraitv2_nom[] = {
 
 static struct acpu_level acpu_freq_tbl_8960_kraitv2_fast[] = {
 	{ 0, { STBY_KHZ, QSB,   0, 0, 0x00 }, L2(0),   850000 },
-        { 1, {   194000, PLL_8, 0, 2, 0x00 }, L2(1),   825000 },
 	{ 1, {   384000, PLL_8, 0, 2, 0x00 }, L2(1),   850000 },
 	{ 0, {   432000, HFPLL, 2, 0, 0x20 }, L2(7),   875000 },
 	{ 1, {   486000, HFPLL, 2, 0, 0x24 }, L2(7),   875000 },
