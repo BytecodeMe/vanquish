987c42904410ae08c3ee2f4d8824bc251674b424
pcarenza <pcarenza@gmail.com>
2013-05-31 15:45:23 -0400

    revert 3.0.79 changes to bluetooth

--- net/bluetooth/af_bluetooth.c
+++ net/bluetooth/af_bluetooth.c
@@ -283,7 +283,6 @@ int bt_sock_recvmsg(struct kiocb *iocb, struct socket *sock,
 	if (flags & (MSG_OOB))
 		return -EOPNOTSUPP;
 
-	msg->msg_namelen = 0;
 
 	skb = skb_recv_datagram(sk, flags, noblock, &err);
 	if (!skb) {
@@ -291,6 +290,8 @@ int bt_sock_recvmsg(struct kiocb *iocb, struct socket *sock,
 			return 0;
 		return err;
 	}
+	
+	msg->msg_namelen=0;
 
 	copied = skb->len;
 	if (len < copied) {
